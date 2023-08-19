1c505163c9f0080dd32013d55d0e41c62a789561
Crowdbotics <team@crowdbotics.com>
2023-08-19 18:22:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wedding-party-track-42951",
+  url: "https://wedding-party-track-42951.botics.co",
+  api: "https://wedding-party-track-42951.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
